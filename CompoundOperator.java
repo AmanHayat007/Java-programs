71335bf0e85d6d83ebde52204bc5886fca7a30ca
AmanHayat007 <111421111+AmanHayat007@users.noreply.github.com>
2023-02-03 20:03:30 +0530

    Type of Operator

---
+++ CompoundOperator.java
@@ -0,0 +1,16 @@
+
+
+
+public class CompoundOperator{
+            public static void main(String[] args) {
+		int x=10, y=10, z=15;
+			x -= 3;
+			y += 5;
+			z /= 3;
+			System.out.println( x );
+			System.out.println( y );
+			System.out.println(z);
+		}
+			
+		}
+		
