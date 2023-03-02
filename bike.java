47d6e57c6414b95ccc46391179cdbbbba06978a0
AmanHayat007 <111421111+AmanHayat007@users.noreply.github.com>
2023-03-02 12:17:40 +0530

    Abstraction

---
+++ bike.java
@@ -0,0 +1,11 @@
+public class bike {
+       final int speed;
+       bike(){
+        speed=100;
+        System.out.println(speed);
+       }
+       public static void main(String args[]){
+        bike b = new bike();
+       }
+
+}
