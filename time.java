f2f3b1f79f162e2ee83a222a3c351c13b48a2aa6
AmanHayat007 <111421111+AmanHayat007@users.noreply.github.com>
2023-04-06 11:46:25 +0530

    Time Method in java

---
+++ time.java
@@ -0,0 +1,9 @@
+import java.time.LocalTime;
+
+public class time {
+    public static void main (String args[]){
+        LocalTime Obj2 = LocalTime.now();
+        System.out.println(Obj2);
+    }
+    
+}
